e4b4993c06964d232d35f4f91b30c187576aac57
Loki <loki@reduxed.dev>
2024-06-17 16:12:48 +1000

    init

---
+++ bin/assetmanager/pcConvert.bat
@@ -0,0 +1,9 @@
+@echo off
+
+@echo This is not recommended. Close this window to cancel.
+pause
+
+::::::::::::::::::::::::::::::::::::
+:: Run Converter
+::::::::::::::::::::::::::::::::::::
+converter
